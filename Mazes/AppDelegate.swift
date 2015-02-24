f081cf646ccc67112fbf2ecf2bec4b6d52cdd981
Ronald Mannak <ronaldmannak@me.com>
2015-02-23 18:56:25 -0800

    Initial Commit

---
+++ Mazes/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Mazes
+//
+//  Created by Ronald Mannak on 2/23/15.
+//  Copyright (c) 2015 Ronald Mannak. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ Mazes/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Mazes
+//
+//  Created by Ronald Mannak on 2/23/15.
+//  Copyright (c) 2015 Ronald Mannak. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
